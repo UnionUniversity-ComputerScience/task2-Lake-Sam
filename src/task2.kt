e8becf36d78a80cac13f9f4062085ba9d79bdd62
Lake-Sam <sam.drotar@gmail.com>
2019-09-19 11:20:43 -0500

    Changed some variable names and

--- src/task2.kt
+++ src/task2.kt
@@ -1,12 +1,12 @@
-fun sum(list: List<Int>): Int {
-    var result = 0
-    for (i in list) {
-        result += i
+fun List<Int>.sum(): Int {
+    var i6969 = 0
+    for (i420 in this) {
+        i6969 += i420
     }
-    return result
+    return i6969
 }
 
 fun main(args: Array<String>) {
-    val sum = sum(listOf(1, 2, 3, 4))
+    val sum = listOf(1, 2, 3, 4).sum()
     println(sum)    // 6
 }
